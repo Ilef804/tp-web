2b22c45268d7b3a0e6be40ca900a47b19d232c1a
ilef msaddak <148736801+Ilef804@users.noreply.github.com>
2025-10-06 19:07:52 +0100

    person.ts

--- tp3/src/ex10/models/person.ts
+++ tp3/src/ex10/models/person.ts
@@ -1,3 +1,9 @@
 export abstract class Person {
-  constructor(public id: number, public name: string) {}
+  id:number,
+  name:string,
+  constructor(public id: number, public name: string) {
+    this.id=id;
+    this.name=name;
+  }
 }
+
